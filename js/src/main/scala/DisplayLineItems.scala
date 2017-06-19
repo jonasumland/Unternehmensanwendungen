b7a4a07efd0e83b5cc6d647b38dcd57f09780e85
Jeilef <justus.eilers@student.hpi.de>
2017-06-19 15:04:59 +0200

    added template to git. now rows are clickable and result in a reaction

--- js/src/main/scala/DisplayLineItems.scala
+++ js/src/main/scala/DisplayLineItems.scala
@@ -22,10 +22,26 @@ import scala.concurrent.ExecutionContext.Implicits.global
 @MarmolataClient(com.sap.marmolata.data.query.untyped.QueryExecAPI)
 object DisplayLineItems extends MarmolataShell {
 
-  val query = sql"select BUKRS, H_BUDAT, BELNR, HKONT, SHKZG, KOART from BSEG"
+  val query = sql"""select kunnr, name1, ort01, pstlz from SAPHPB.KNA1
+    where contains(kunnr, '', fuzzy(0.1)) and contains(name1, '', fuzzy(0.1)) and  contains(ort01, '', fuzzy(0.1)) and contains(pstlz, '', fuzzy(0.1))"""
   val filter = FilterBar.datasource(query).build
 
-  val table = Table.datasource(filter.output).selectionMode(SelectionMode.None).build
+  type Row = (String, String, Int)
+  val input:Table[Row] =
+    Table[Row]()
+      .title(Label("Available persons").build)
+      .visibleRowCountMode(VisibleRowCountMode.Fixed)
+      .selectionMode(SelectionMode.Single)
+      .columns(
+        Column().headingWithLabel("Name").controlFromText((a: Row) => a._1).build,
+        Column().headingWithLabel("City").controlFromText((a: Row) => a._2).build,
+        Column().headingWithLabel("Age").controlFromText((a: Row) => a._3.toString).build
+      )
+      .content(StaticRowProvider(Seq(("Hans", "Potsdam", 24), ("Julia", "Berlin", 23)))).build()
+  val helloLabel = Label(
+    input.selectedRows.map(rows => {
+      rows.headOption.map(row => s"hello ${row._1}").getOrElse("")
+    })).build
 
-  val render = App().initialPage(Page().content(filter above table)).build
+  val render = App().initialPage(Page().content(filter above helloLabel above input)).build
 }
