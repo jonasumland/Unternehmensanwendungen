14f1d132f1027203bdf0a72ec52062083481daf4
Jeilef <justus.eilers@student.hpi.de>
2017-06-22 14:16:18 +0200

    leere Seite

--- js/src/main/scala/DisplayLineItems.scala
+++ js/src/main/scala/DisplayLineItems.scala
@@ -27,40 +27,27 @@ object DisplayLineItems extends MarmolataShell {
 
   val table = Table.datasource(filter.output).selectionMode(SelectionMode.Single).build
 
-  type Row = (String, String, String, String)
+  val kunnr = table.selectedRows.map(rows => {
+    rows.headOption.map(row => s"SELECT KUNNR FROM KNA1 WHERE KUNNR = '${row.KUNNR.value}'").getOrElse(s"SELECT KUNNR FROM KNA1 WHERE KUNNR = 'NULL'")})
 
-  val input:Table[Row] =
-    Table[Row]()
-      .visibleRowCountMode(VisibleRowCountMode.Fixed)
-      .selectionMode(SelectionMode.Single)
-      .columns(
-        Column().heading("KUNNR").controlFromText((a: Row) => a._1).build,
-        Column().heading("NAME1").controlFromText((a: Row) => a._2).build,
-        Column().heading("ORT01").controlFromText((a: Row) => a._3).build,
-        Column().heading("PSTLZ").controlFromText((a: Row) => a._4).build
-      )
-      .content(filter.output).build()
+  val selectedColOfRow: Signal[Option[String]] = table.selectedRows.map(_.headOption.map(_.KUNNR.value))
+  val query2 = sql"select KUNNR , NAME1,ORT01,PSTLZ from KNA1 where KUNNR=${selectedColOfRow.map(_.getOrElse(""))}"
+  val filter2 = FilterBar.datasource(query2).build
+  val tableTarget = Table.datasource(filter2.output).selectionMode(SelectionMode.None).build
 
-  val helloLabel = Label(
-    input.selectedRows.map(rows => {
-      rows.headOption.map(row => s"hello ${row._1}").getOrElse("")
-    })).build
-
+    val button = Button().text("Go to page 2").build
 
   //val render = App().initialPage(Page().content(filter above table)).build
 
-  val button = Button().text("Go to page 2").build
-  
-    table.selectedRows.observe(x => println(x.map( row => row.KUNNR.value).mkString(",")))
-    button.clicks.observe(_ => pageTransitions := PageTransition(StaticBuilder(page2)))
+
+
 //    page2.navButtonPress.observe(_ => pageTransitions := PageTransition(StaticBuilder(page1.build()), PageTransitionEffect.SlideRight))
 
   val page1 = Page().title("Page 1").content(button above filter above table).build()
-  val page2 = Page().title("Page 2").showNavButton(true).content(Label("Hello 2")).build()
+  val page2 = Page().title("Page 2").showNavButton(true).content(tableTarget).build()
 
 
   val pageTransitions: EventSource[PageTransition] = EventSource()
-
 
   button.clicks.observe(_ => pageTransitions := PageTransition(StaticBuilder(page2)))
   page2.navButtonPress.observe(_ => pageTransitions := PageTransition(StaticBuilder(page1), PageTransitionEffect.SlideRight))
@@ -70,22 +57,6 @@ object DisplayLineItems extends MarmolataShell {
 /*  val query = sql"""select KUNNR, NAME1, ORT01, PSTLZ from KNA1"""  //contains(KUNNR, '', fuzzy(0.1) and contains(name1, '', fuzzy(0.1)) and  contains(ort01, '', fuzzy(0.1)) and contains(pstlz, '', fuzzy(0.1))"""
   val filter = FilterBar.datasource(query).build
 
-  type Row = (String, String, Int)
-  val input:Table[Row] =
-    Table[Row]()
-      .title(Label("Available persons").build)
-      .visibleRowCountMode(VisibleRowCountMode.Fixed)
-      .selectionMode(SelectionMode.Single)
-      .columns(
-        Column().headingWithLabel("Name").controlFromText((a: Row) => a._1).build,
-        Column().headingWithLabel("City").controlFromText((a: Row) => a._2).build,
-        Column().headingWithLabel("Age").controlFromText((a: Row) => a._3.toString).build
-      )
-      .content(StaticRowProvider(Seq(("Hans", "Potsdam", 24), ("Julia", "Berlin", 23)))).build()
-  val helloLabel = Label(
-    input.selectedRows.map(rows => {
-      rows.headOption.map(row => s"hello ${row._1}").getOrElse("")
-    })).build
 
   val render = App().initialPage(Page().content(filter above helloLabel above input)).build*/
 }
